a7145d03d3f0855aa4d31da62bd4b993d604cbe0
Nguyen Huu Vu <vu.nguyen@rikai.technology>
2022-07-15 13:28:44 +0700

    convert Hilt

--- app/src/main/java/com/vunh/recycler_hilt/BaseApp.kt
+++ app/src/main/java/com/vunh/recycler_hilt/BaseApp.kt
@@ -3,8 +3,6 @@ package com.vunh.recycler_hilt
 import android.app.Application
 import com.vunh.recycler_hilt.api.RecyclerService
 import com.vunh.recycler_hilt.database.AppDatabase
-import com.vunh.recycler_hilt.di.AppComponent
-import com.vunh.recycler_hilt.di.DaggerAppComponent
 import com.vunh.recycler_hilt.repository.RecyclerRepositoryImpl
 import dagger.hilt.android.HiltAndroidApp
 
